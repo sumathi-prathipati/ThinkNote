3c100ef2e731553b2213e8e839a425d88437f056
sumathi prathipati <prathipatisumathi5@gmail.com>
2026-09-22 23:11:04 +0530

    Update CORS origin to include new URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
   app.use(
     cors({
       origin: ["http://localhost:5173",
-     "https://think-note-g1ew51rcy-sumathi2.vercel.app/"
+     "https://think-note-eight.vercel.app/"
       ],
             methods: ["GEY", "POST", "PUT", "DELETE", "OPTIONS"],
       credentials: true
